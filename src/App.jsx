92cb25dd24ede6a8c5c93749ee959e5aa563516a
Michael Lee <mhouyin0210@gmail.com>
2026-04-01 17:30:44 -0400

    Added basic search functionality and implemented OpenWeather API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  const [city, setCity] = useState('')
+  const [searchedCity, setSearchedCity] = useState('')
+
+  const handleSearch = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7ad89fce01f5fe32a29d42fdfbcd3b1a&units=metric`
+    )
+
+    const data = await response.json()
+
+    console.log(data)
+
+    setSearchedCity(city)
+  }
+
   return (
     <div>
       <h1>SkyCast</h1>
-      <p>Weather app coming soon</p>
+
+      <input
+        type="text"
+        placeholder="Enter city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)} 
+      />
+
+      <button onClick={handleSearch}>Search</button>
+
+      {searchedCity && <p>City: {searchedCity}</p>}
     </div>
   );
 }
